f0b62f49cfbd77d8dd74ed93b4b7d040cc334bfd
nataliaaba3 <nataliaaba3@outlook.com>
2023-09-19 21:31:58 -0300

    tercera copia

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,6 @@
 console.log(3+3);
-        console.log( "MARCELA" + " " + "CERDA" )
-        console.log( "edad:" + (15 + 3))
-        console.info ("informacion al usuario")
-        console.error ("atencion")
-        console.warn ("cuidado")
-        
+        console.log( "MARCELA" + " " + "CERDA" );
+        console.log( "edad:" + (15 + 3));
+        console.info ("informacion al usuario");
+        console.error ("atencion");
+        console.warn ("cuidado");
