1c8e284d6ecdaf798d2df29e78114f7860c5b1ab
nataliaaba3 <nataliaaba3@outlook.com>
2023-09-19 22:50:46 -0300

    cuarta copia

--- JS/main.js
+++ JS/main.js
@@ -4,3 +4,31 @@ console.log(3+3);
         console.info ("informacion al usuario");
         console.error ("atencion");
         console.warn ("cuidado");
+       // alert("este es un mensaje de alerta"); // crea una ventana con el mensaje
+// comentario de linea
+
+/*comentario
+de bloque para 
+varias lineas*/
+
+let cliente="Juan"
+let total=0
+let producto1=""
+let _num=0
+let porcentajeDescuento=15;
+let nombreDelCliente=""
+
+var s = "Hola, me llamo Juan"; // s, de string
+var n = 28; // n, de número
+var b = true; // b, de booleano
+var u; // u, de undefined
+console.log(typeof(s)); //String
+console.log(typeof(n)); //number
+console.log(typeof(b));
+console.log(typeof(u));
+document.write("Escrito desde JS");
+document.write("<h2>Escrito desde JS</h2>");
+let nombre=prompt("Ingrese su nombre");
+console.log("El nombre ingresado por el usuario es :" + nombre)
+
+
